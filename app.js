77d80c9617e57e05771d43effc1ec481580c51aa
1Aksoni <akshat2001soni@gmail.com>
2024-10-02 16:52:23 +0530

    allow cors to verscel

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/Customer.routes.js";
 import homeDataRouter from './routes/HomeData.routes.js';
 const app = express();
-
-app.use(cors()); // Enable CORS
+const allowedOrigins = ['https://heven-hub-frontend.vercel.app']; 
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true // Allow credentials (cookies, authorization headers, etc.)
+  })); // Enable CORS
 app.use(express.json()); // Convert incoming JSON to JS objects
 app.use(express.urlencoded({ extended: true })); // Added `extended: true` for complex objects
 app.use(express.static("public"));
